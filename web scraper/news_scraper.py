10c4537e1f8e10fc7be2272c7b42149ad903bad6
muskaancodes-hash <muskaan.kumari7908@gmail.com>
2026-09-01 15:45:43 +0530

    Add files via upload
    we added BeautifulSoup to read and parse the website's HTM

--- web scraper/news_scraper.py
+++ web scraper/news_scraper.py
@@ -6,3 +6,19 @@ response = requests.get(url)
 
 print(response.status_code)
 print(response.text[:500])
+import requests
+from bs4 import BeautifulSoup
+
+# Website URL
+url = "https://www.bbc.com/news"
+
+# Send a request to the website
+response = requests.get(url)
+
+# Check whether the request was successful
+print("Status Code:", response.status_code)
+
+# Parse the HTML content
+soup = BeautifulSoup(response.text, "html.parser")
+
+print("HTML content parsed successfully!")
