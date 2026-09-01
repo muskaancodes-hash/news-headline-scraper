61129101b459df2b7638f3ff2ba9f91c1b5b0253
muskaancodes-hash <muskaan.kumari7908@gmail.com>
2026-09-01 15:53:21 +0530

    Add files via upload

---
+++ web scraper/web scraper/news_scraper.py
@@ -0,0 +1,30 @@
+import requests
+
+url = "https://www.bbc.com/news"
+
+response = requests.get(url)
+
+print(response.status_code)
+print(response.text[:500])
+import requests
+from bs4 import BeautifulSoup
+
+# Website URL
+url = "https://www.bbc.com/news"
+
+# Send a request to the website
+response = requests.get(url)
+
+# Check whether the request was successful
+print("Status Code:", response.status_code)
+
+# Parse the HTML content
+soup = BeautifulSoup(response.text, "html.parser")
+
+print("HTML content parsed successfully!")
+# Find all h2 tags from the webpage
+headlines = soup.find_all("h2")
+
+# Print each headline
+for headline in headlines:
+    print(headline.text.strip())
